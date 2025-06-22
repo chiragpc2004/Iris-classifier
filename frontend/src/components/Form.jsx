a5a922c5d1237d6ad12324a1c339f173f977d49b
chiragpc2004 <chiragpc2004@gmail.com>
2025-06-22 16:53:41 +0530

    connect

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -19,7 +19,7 @@ export default function Form() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://127.0.0.1:8000/predict", {
+      const res = await fetch("https://iris-9c9f.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
